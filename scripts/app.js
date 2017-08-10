8dca9280556bbf533c48ac5a30f2125622a56c8d
julianfcp <julian.fcp@gmail.com>
2017-08-10 15:16:05 -0500

    Adding some services

--- scripts/app.js
+++ scripts/app.js
@@ -4,25 +4,40 @@ angular.module("todoListApp", [])
 
   $scope.helloConsole = dataService.helloConsole;
 
-
   $scope.helloWorld = function() {
     console.log("Hello there! This the helloWorld controller function, in the mainCtrl!");
   };
-  $scope.todos = [
-    {"name": "Clean the house"},
-    {"name": "Study english"},
-    {"name": "Get some food"},
-    {"name": "Make the CV file"},
-    {"name": "other todo"},
-    {"name": "Make love"},
-    {"name": "Drink more beer"},
-  ]
+  // sends callback function so scope.todos loads its values after
+  dataService.getTodos(function(response) {
+    console.log(response.data);
+    $scope.todos = response.data;
+  });
+  // delete functions receive index from ng-repeat to delete the item
+  $scope.deleteTodo = function(todo, $index){
+    dataService.deleteTodo(todo);
+    //splice function removes 1 item from the todos list starting from index
+    $scope.todos.splice($index, 1);
+  };
+  $scope.saveTodo = function(todo){
+    dataService.saveTodo(todo);
+  };
   
 })
 
-.service('dataService', function() {
+.service('dataService', function($http) {
     this.helloConsole = function () {
         console.log('this is the hello console service!');
     }
+    // receive callback function  after loading data
+    this.getTodos = function(callback) {
+        $http.get('mock/todos.json').then(callback)
+    };
+    this.deleteTodo = function(todo){
+        console.log("The " + todo.name + " todo has been deleted!");
+    };
+    this.saveTodo = function(todo){
+            console.log("The " + todo.name + " todo has been saved!");
+        };
+
 
 });
